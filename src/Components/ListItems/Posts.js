dd2f219be7cd37b6aec5193f0ca3ce9770b408bb
JustasBan <justas.ban2001@gmail.com>
2022-05-15 23:35:54 +0300

    Searhing
    Added searching feature with id, user id, body, title, which filters posts according to queries
    UI included

--- src/Components/ListItems/Posts.js
+++ src/Components/ListItems/Posts.js
@@ -7,7 +7,6 @@ const CurrentPosts = ({ items }) => (
     post => <IndividualPost key={post.id} userId={post.userId} id={post.id} title={post.title} body={post.body} />
   )
 )
-
 const Posts = ({ postsArg }) => {
   let sortState = ({ key }) => {
     switch (sortKeys[key]) {
@@ -17,7 +16,7 @@ const Posts = ({ postsArg }) => {
         return "(ascend.)"
 
       default:
-      return null
+        return null
     }
   }
 
@@ -28,13 +27,14 @@ const Posts = ({ postsArg }) => {
   const [currentItems, setCurrentItems] = useState(postsArg);
   const [posts, setPosts] = useState(postsArg);
   const [pageCount, setPageCount] = useState(0);
+  const [regen, setRegen] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
 
   useEffect(() => {
     const endOffset = itemOffset + itemsPerPage;
     setCurrentItems(posts.slice(itemOffset, endOffset));
     setPageCount(Math.ceil(posts.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage, sortKeys]);
+  }, [itemOffset, itemsPerPage, sortKeys, regen]);
 
   const handlePageClick = (event) => {
     const newOffset = event.selected * itemsPerPage % posts.length;
@@ -67,23 +67,74 @@ const Posts = ({ postsArg }) => {
   const handleTitle = () => {
     if (sortKeys.title) {
       setSortKeys((x) => ({ ...x, title: false }))
-      setPosts((x) => (x.sort((a, b) => b.title > a.title)))
+      setPosts((x) => (x.sort((a, b) => a.title > b.title)))
     }
     else {
       setSortKeys((x) => ({ ...x, title: true }))
-      setPosts((x) => (x.sort((a, b) => a.title > b.title)))
+      setPosts((x) => (x.sort((a, b) => b.title > a.title)))
     }
   }
 
   const handleBody = () => {
     if (sortKeys.body) {
       setSortKeys((x) => ({ ...x, body: false }))
-      setPosts((x) => (x.sort((a, b) => b.body > a.body)))
+      setPosts((x) => (x.sort((a, b) => a.body > b.body)))
     }
     else {
       setSortKeys((x) => ({ ...x, body: true }))
-      setPosts((x) => (x.sort((a, b) => a.body > b.body)))
+      setPosts((x) => (x.sort((a, b) => b.body > a.body)))
     }
+  }
+
+  //FILTER PART
+  const [input, setInput] = useState(
+    {
+      id: '',
+      userId: '',
+      title: '',
+      body: ''
+    }
+  )
+
+  const handleInputChange = (event) => {
+    let newerInput
+    switch (event.target.className) {
+
+      case 'title':
+        newerInput = input
+        setInput({ ...newerInput, title: event.target.value })
+        break
+
+      case 'body':
+        newerInput = input
+        setInput({ ...newerInput, body: event.target.value })
+        break
+
+      case 'id':
+        newerInput = input
+        setInput({ ...newerInput, id: event.target.value })
+        break
+
+      case 'userId':
+        newerInput = input
+        setInput({ ...newerInput, userId: event.target.value })
+        break
+
+      default:
+        break
+    }
+  }
+
+  const search = (event) => {
+    event.preventDefault()
+    setPosts(postsArg.filter(
+      p =>(
+          p.title.includes(input.title) &&
+          p.body.includes(input.body) &&
+          String(p.id).includes(input.id) &&
+          String(p.userId).includes(input.userId)
+        )))
+    setRegen((x) => x + 1)
   }
 
   return (
@@ -94,10 +145,10 @@ const Posts = ({ postsArg }) => {
       <table className='centerComponent'>
         <tbody>
           <tr>
-            <td onClick={() => handleUserId()}>userId {sortState({key: "userId"})}</td>
-            <td onClick={() => handleId()}>id {sortState({key: "id"})}</td>
-            <td onClick={() => handleTitle()}>title {sortState({key: "title"})}</td>
-            <td onClick={() => handleBody()}>body {sortState({key: "body"})}</td>
+            <td onClick={() => handleUserId()}>userId {sortState({ key: "userId" })}</td>
+            <td onClick={() => handleId()}>id {sortState({ key: "id" })}</td>
+            <td onClick={() => handleTitle()}>title {sortState({ key: "title" })}</td>
+            <td onClick={() => handleBody()}>body {sortState({ key: "body" })}</td>
           </tr>
 
           <CurrentPosts items={currentItems} />
@@ -125,6 +176,47 @@ const Posts = ({ postsArg }) => {
       </table>
 
       <div id="container" />
+
+      <h3 className='centerComponent'>Search</h3>
+
+      <form onSubmit={search}>
+        <div className='formsWrap' >
+          <div>
+            <h3>Id:</h3>
+            <div className='formsWrapInner' style={{marginRight: 25}}>
+              <textarea className='id' onChange={handleInputChange} />
+            </div>
+          </div>
+
+          <div>
+            <h3>UserId:</h3>
+            <div className='formsWrapInner'>
+              <textarea className='userId' onChange={handleInputChange} />
+            </div>
+          </div>
+        </div>
+
+        <div className='formsWrap'>
+          <div >
+            <h3>Title:</h3>
+            <div className='formsWrapInner' style={{marginRight: 25}}>
+              <textarea className='title' onChange={handleInputChange} />
+            </div>
+          </div>
+
+          <div>
+            <h3>Body:</h3>
+            <div className='formsWrapInner'>
+              <textarea className='body' onChange={handleInputChange} />
+            </div>
+          </div>
+        </div>
+
+        <div className='postWrap'>
+          <button className='formsButton' type="submit">Request</button>
+        </div>
+      </form>
+
     </div>
   )
 }
